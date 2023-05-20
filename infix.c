b2b2c5145d8daef856909358174b5695ccaa7620
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2023-05-20 01:34:37 -0300

    Add helper functions

--- infix.c
+++ infix.c
@@ -1,10 +1,32 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define INFIX_EXPR_MAX_SIZE 256
 
 typedef struct {
 	char expr[INFIX_EXPR_MAX_SIZE];
 } Infix;
+
+void assert_msg(bool condition, char msg[]) {
+    if (!condition) {
+        printf("%s", msg);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void insert_char(char* str, char ch, int index) {
+    assert_msg(strlen(str) + 1 < 100, "String length exceeded size limit!\n");
+    assert_msg(index < strlen(str), "Index exceeded string length!\n");
+
+    // Shift everything to the right from and including index.
+    for (int i = strlen(str); i >= index; i--) {
+        str[i + 1] = str[i];
+    }
+
+    str[index] = ch;
+}
 
 Infix infix_new(char expr[INFIX_EXPR_MAX_SIZE]) {
 	Infix infix;
@@ -13,6 +35,4 @@ Infix infix_new(char expr[INFIX_EXPR_MAX_SIZE]) {
 	return infix;
 }
 
-Infix infix_parenthesize(Infix infix) {
-	
-}
+Infix infix_parenthesize(Infix infix);

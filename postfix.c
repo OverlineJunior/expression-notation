716cdf4da78add6acee1b194f2ba203eb0da5680
over <franciscosebastianyjunior@hotmail.com>
2023-05-25 19:08:21 -0300

    Define and implement postfix_new and remove postfix_from's temporary implementation

--- postfix.c
+++ postfix.c
@@ -1,15 +1,20 @@
 #include "infix.h"
 #include "string.h"
 
-#define POSTFIX_EXPR_MAX_SIZE 256
+#define POSTFIX_EXPR_SIZE 256
 
 typedef struct {
-	char expr[POSTFIX_EXPR_MAX_SIZE];
+	char expr[POSTFIX_EXPR_SIZE];
 } Postfix;
 
-Postfix postfix_from(Infix infix) {
-	Postfix postfix;
-	strcpy_s(postfix.expr, POSTFIX_EXPR_MAX_SIZE, infix.expr);
+Postfix postfix_new(char expr[POSTFIX_EXPR_SIZE]) {
+	char sized_expr[POSTFIX_EXPR_SIZE];
+    strcpy(sized_expr, expr);
 
+	Postfix postfix;
+	strcpy(postfix.expr, sized_expr);
+    
 	return postfix;
 }
+
+Postfix postfix_from(Infix infix);

ea3ed2df3125e20d2580b75091affcc29a938cd3
over <franciscosebastianyjunior@hotmail.com>
2023-05-31 21:02:20 -0300

    Define and implement both is_operator and is_operand, and start implementing infix_to_postfix

--- expression-notation.c
+++ expression-notation.c
@@ -1,1 +1,26 @@
-void infix_to_postfix(char* expr);
+#include <string.h>
+#include "libs/stack.h"
+
+bool is_operator(char ch) {
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+}
+
+bool is_operand(char ch) {
+    return !is_operator(ch) && ch != ' ' && ch != '(' && ch != ')';
+}
+
+void infix_to_postfix(char* expr) {
+    Stack operators = stack_new();
+    char buffer[strlen(expr)]; // TODO: Don't forget to add \0 at the end.
+
+    for (int i = 0; i < strlen(expr); i++) {
+        const char token = expr[i];
+
+        if (is_operand(token))
+            strcat(buffer, &token);
+
+        
+    }
+
+    strcpy(expr, buffer);
+}
--- test/main.c
+++ test/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
 #include "../expression-notation.h"
 
 int main(void) {
-    
+    char expr[] = "(1 + 2) * 3";
+    printf("%s\n", expr);
+    infix_to_postfix(expr);
+    printf("%s\n", expr);
 }

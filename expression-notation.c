736efd28e59df29c0b444ed7e13005df6c4b0eeb
over <franciscosebastianyjunior@hotmail.com>
2023-06-01 20:53:26 -0300

    Implement infix_to_postfix and add testings (third test failing)

--- expression-notation.c
+++ expression-notation.c
@@ -1,25 +1,119 @@
 #include <string.h>
 #include "libs/stack.h"
 
-bool is_operator(char ch) {
-    return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+#define NONE -1
+
+void push_char(char* dest, char ch) {
+    int len = strlen(dest);
+
+    dest[len] = ch;
+    dest[len + 1] = '\0';
 }
 
-bool is_operand(char ch) {
-    return !is_operator(ch) && ch != ' ' && ch != '(' && ch != ')';
+bool is_operator(char token) {
+    return token == '+' || token == '-' || token == '*' || token == '/';
 }
 
+bool is_operand(char token) {
+    return !is_operator(token) && token != ' ' && token != '(' && token != ')';
+}
+
+int input_precedence(char token) {
+    if (token == ')') {
+        return 0;
+    } else if (token == '(') {
+        return 5;
+    } else if (token == '+' || token == '-') {
+        return 1;
+    } else if (token == '*' || token == '/') {
+        return 3;
+    }
+
+    return NONE;
+}
+
+int stack_precedence(char token) {;
+    if (token == '(') {
+        return 0;
+    } else if (token == '+' || token == '-') {
+        return 2;
+    } else if (token == '*' || token == '/') {
+        return 4;
+    }
+
+    return NONE;
+}
+
+/*
 void infix_to_postfix(char* expr) {
-    Stack operators = stack_new();
-    char buffer[strlen(expr)]; // TODO: Don't forget to add \0 at the end.
+    Stack stack = stack_new();
+    char buffer[strlen(expr)];
+
+    // Removes a garbage value at index 0.
+    strcpy(buffer, "");
 
     for (int i = 0; i < strlen(expr); i++) {
         const char token = expr[i];
 
-        if (is_operand(token))
-            strcat(buffer, &token);
+        if (is_operand(token)) {
+            push_char(buffer, token);
+        } else if (is_operator(token)) {
+            while (
+                !stack_is_empty(stack) &&
+                is_operator(stack_peek(stack)) &&
+                get_precedence(stack_peek(stack)) > get_precedence(token)
+            ) {
+               push_char(buffer, stack_pop(&stack));
+            }
 
-        
+            stack_push(&stack, token);
+        } else if (token == '(') {
+            stack_push(&stack, token);
+        } else if (token == ')') {
+            while (!stack_is_empty(stack)) {
+                const char top = stack_pop(&stack);
+
+                if (top == '(')
+                    break;
+
+                push_char(buffer, top);
+            }
+        }
+    }
+
+    while (!stack_is_empty(stack))
+        push_char(buffer, stack_pop(&stack));
+
+    strcpy(expr, buffer);
+}
+*/
+
+void infix_to_postfix(char* expr) {
+    Stack stack = stack_new();
+    stack_push(&stack, '(');
+
+    push_char(expr, ')');
+
+    char buffer[strlen(expr)];
+    // Removes a garbage value at index 0.
+    strcpy(buffer, "");
+
+    for (int i = 0; i < strlen(expr); i++) {
+        const char token = expr[i];
+
+        if (is_operand(token)) {
+            push_char(buffer, token);
+        } else {
+            while (!stack_is_empty(stack) && stack_precedence(stack_peek(stack)) >= input_precedence(token)) {
+                const char top = stack_pop(&stack);
+
+                if (top != '(')
+                    push_char(buffer, top);
+            }
+
+            if (token != ')')
+                stack_push(&stack, token);
+        }
     }
 
     strcpy(expr, buffer);
--- test/main.c
+++ test/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include "../expression-notation.h"
 
+void test_infix_to_postifix(char input[], char expected_output[]) {
+    char output[strlen(input)];
+    strcpy(output, input);
+
+    infix_to_postfix(output);
+
+    if (strcmp(output, expected_output) == 0)
+        printf("Test passed\n");
+    else
+        printf("Expected `%s`, got `%s`\n", expected_output, output);
+}
+
 int main(void) {
-    char expr[] = "(1 + 2) * 3";
-    printf("%s\n", expr);
-    infix_to_postfix(expr);
-    printf("%s\n", expr);
+    test_infix_to_postifix("(A+B*C)", "ABC*+");
+    test_infix_to_postifix("(A*(B+C)/D-E)", "ABC+*D/E-");
+    test_infix_to_postifix("(A+B*(C-D*(E-F)-G*H)-I*3)", "ABCDEF-*-GH*-*+I3*-");
+    test_infix_to_postifix("(A+B*C/D*E-F)", "ABC*D/E*+F-");
+    test_infix_to_postifix("(A+B+C*D-E*F*G)", "AB+CD*+EF*G*-");
+    test_infix_to_postifix("(A+(B-(C+(D-(E+F)))))", "ABCDEF+-+-+");
+    test_infix_to_postifix("(A*(B+(C*(D+(E*(F+G))))))", "ABCDEFG+*+*+*");
 }

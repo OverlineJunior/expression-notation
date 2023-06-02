47e44a3dfee80a5cd6033b092a01426be233cb59
over <franciscosebastianyjunior@hotmail.com>
2023-06-02 15:51:29 -0300

    Comment some stack functions

--- libs/stack.c
+++ libs/stack.c
@@ -17,6 +17,7 @@ typedef struct {
     int top_index;
 } Stack;
 
+// Returns a new empty stack.
 Stack stack_new(void) {
     Stack stack = { .top_index = INT_NONE };
     return stack;
@@ -30,18 +31,24 @@ bool stack_is_empty(Stack stack) {
     return stack.top_index == INT_NONE;
 }
 
+/// @brief Appends `value` to the top of the stack.
+/// @exception Cannot push to full stack.
 void stack_push(Stack* stack, char value) {
     assert_msg(!stack_is_full(*stack), "Cannot push to full stack!");
 
     stack->elements[++stack->top_index] = value;
 }
 
+/// @brief Removes the top element from the stack.
+/// @return Value popped.
+/// @exception Cannot pop from empty stack.
 char stack_pop(Stack* stack) {
     assert_msg(!stack_is_empty(*stack), "Cannot pop from empty stack!");
 
     return stack->elements[stack->top_index--];
 }
 
+// Returns the top element from the stack without popping it out.
 char stack_peek(Stack stack) {
     assert_msg(!stack_is_empty(stack), "Cannot peek empty stack!");
 

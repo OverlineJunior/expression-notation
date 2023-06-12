522be8fcb7313794846c2d70b568f04d3d643827
over <franciscosebastianyjunior@hotmail.com>
2023-06-12 20:33:31 -0300

    Fix typo

--- test/main.c
+++ test/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "../expression-notation.h"
 
-void test_infix_to_postifix(char input[], char expected_output[]) {
+void test_infix_to_postfix(char input[], char expected_output[]) {
     char* output = malloc(strlen(input) + 1);
     strcpy(output, input);
 
@@ -18,11 +18,11 @@ void test_infix_to_postifix(char input[], char expected_output[]) {
 }
 
 int main(void) {
-    test_infix_to_postifix("(A+B*C)", "ABC*+");
-    test_infix_to_postifix("(A*(B+C)/D-E)", "ABC+*D/E-");
-    test_infix_to_postifix("(A+B*(C-D*(E-F)-G*H)-I*3)", "ABCDEF-*-GH*-*+I3*-");
-    test_infix_to_postifix("(A+B*C/D*E-F)", "ABC*D/E*+F-");
-    test_infix_to_postifix("(A+B+C*D-E*F*G)", "AB+CD*+EF*G*-");
-    test_infix_to_postifix("(A+(B-(C+(D-(E+F)))))", "ABCDEF+-+-+");
-    test_infix_to_postifix("(A*(B+(C*(D+(E*(F+G))))))", "ABCDEFG+*+*+*");
+    test_infix_to_postfix("(A+B*C)", "ABC*+");
+    test_infix_to_postfix("(A*(B+C)/D-E)", "ABC+*D/E-");
+    test_infix_to_postfix("(A+B*(C-D*(E-F)-G*H)-I*3)", "ABCDEF-*-GH*-*+I3*-");
+    test_infix_to_postfix("(A+B*C/D*E-F)", "ABC*D/E*+F-");
+    test_infix_to_postfix("(A+B+C*D-E*F*G)", "AB+CD*+EF*G*-");
+    test_infix_to_postfix("(A+(B-(C+(D-(E+F)))))", "ABCDEF+-+-+");
+    test_infix_to_postfix("(A*(B+(C*(D+(E*(F+G))))))", "ABCDEFG+*+*+*");
 }
